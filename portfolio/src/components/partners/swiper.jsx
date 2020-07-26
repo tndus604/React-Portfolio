cdb3d202670853a6d351311843e6ef2760281714
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-07-26 16:42:23 -0400

    added tech icons

--- portfolio/src/components/partners/swiper.jsx
+++ portfolio/src/components/partners/swiper.jsx
@@ -5,38 +5,38 @@ import "swiper/css/swiper.css";
 // Components
 import PartnerBox from "./partnerBox";
 // Assets
-import Partner01 from "../../assets/partners/partner01.svg";
-import Partner02 from "../../assets/partners/partner02.svg";
-import Partner03 from "../../assets/partners/partner03.svg";
-import Partner04 from "../../assets/partners/partner04.svg";
-import Partner05 from "../../assets/partners/partner05.svg";
-import Partner06 from "../../assets/partners/partner06.svg";
+import HTML5 from "../../assets/partners/HTML5.png";
+import CSS3 from "../../assets/partners/CSS3.png";
+import javascript from "../../assets/partners/javascript.png";
+import mysql from "../../assets/partners/mysql.png";
+import mongodb from "../../assets/partners/mongodb.png";
+import react from "../../assets/partners/react.png";
 
 class MutipleSlidesPerView extends React.Component {
   state = {
     partners: [
       {
-        preview: Partner01,
+        preview: HTML5,
         id: "1",
       },
       {
-        preview: Partner02,
+        preview: CSS3,
         id: "2",
       },
       {
-        preview: Partner03,
+        preview: javascript,
         id: "3",
       },
       {
-        preview: Partner04,
+        preview: mysql,
         id: "4",
       },
       {
-        preview: Partner05,
+        preview: mongodb,
         id: "5",
       },
       {
-        preview: Partner06,
+        preview: react,
         id: "6",
       },
     ],
@@ -46,9 +46,9 @@ class MutipleSlidesPerView extends React.Component {
     let partnersRender = null;
 
     if (this.state.partners) {
-      partnersRender = this.state.partners.map((partner) => (
-        <div key={partner.id}>
-          <PartnerBox partner={partner.preview} />
+      partnersRender = this.state.partners.map((partners) => (
+        <div key={partners.id}>
+          <PartnerBox partner={partners.preview} />
         </div>
       ));
     }
